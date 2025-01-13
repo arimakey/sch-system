f523264bfc2d334e72e01c44832a0d65a8abb476
ArimaKey <127151745+arimakey@users.noreply.github.com>
2025-01-13 16:17:28 -0500

    feat: se agregaron proyectos en nest y en remix

---
+++ apps/client/jest.config.ts
@@ -0,0 +1,9 @@
+export default {
+  displayName: 'client',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^.+\\.[tj]sx?$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: 'test-output/jest/coverage',
+};
